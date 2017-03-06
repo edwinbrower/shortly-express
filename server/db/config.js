cd668c6409a6c68f03e1f2f9276ce9c67a517d5b
edwinbrower <edwin.brower@gmail.com>
2017-03-06 15:26:42 -0800

    Create schema for users and sessions

--- server/db/config.js
+++ server/db/config.js
@@ -26,6 +26,23 @@ module.exports = function(db) {
   /************************************************************/
   /*          Add additional schema queries here              */
   /************************************************************/
+  .then(function() {
+    return db.queryAsync('CREATE TABLE IF NOT EXISTS users (\
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+      username VARCHAR(12) NOT NULL UNIQUE,\
+      password VARCHAR(16) NOT NULL\
+      );'
+    );
+  })
+  .then(function() {
+    return db.queryAsync('CREATE TABLE IF NOT EXISTS sessions (\
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+      hash VARCHAR(20) NOT NULL,\
+      user_id INT NOT NULL,\
+      FOREIGN KEY(user_id) references users (id)\
+      );'
+    );
+  })
 
   .error(function(err) {
     console.log(err);
