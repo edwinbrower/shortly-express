6d3378a7f5fff39cfec56b592f17ddbe5088353d
edwinbrower <edwin.brower@gmail.com>
2017-03-07 14:13:46 -0800

    Work on sessions parser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,39 +1,16 @@
-// var Sessions = require('../models/session');
 var createSession = require('./sessionParser');
 
 var parseCookies = function(req, res, next) {
   console.log('C is for Cookie: ', req.headers.cookie);
   var cookieString = req.headers.cookie;
-  // console.log('req cookie', req.head)
   if (cookieString) {
     var cookieArray = cookieString.split(';');
-    // console.log('A single: ', cookieArray);
     cookieArray.forEach(function(cookie) {
       var hash = cookie.split('=');
-      // console.log('hash', hash);
       req.cookies[hash[0].trim()] = hash[1];
-      // IDEA:
-      // save user id and hash? into sessions database
-      // Sessions.insertSession([hash[1], hash[0]]);
     });
-    createSession(req, res, next);
-    // console.log('hans test', req.cookies);
   }
-  // if there are no cookies call next
-  // else call sessionParser
-  // next('');
-  // if (req.cookies) {
-  //   createSession(req, res, next);
-  // } else {
-  //   next();
-  // }
-  next();
+  createSession(req, res, next);
 };
 
 module.exports = parseCookies;
-
-// do we need to add it to the session db at this point??? our initial thought was no.
-
-// var shasum = crypto.createHash('sha1');
-// shasum.update(userParams[1]);
-// userParams[1] = shasum.digest('hex');
--- server/middleware/sessionParser.js
+++ server/middleware/sessionParser.js
@@ -1,12 +1,31 @@
 var Sessions = require('../models/session');
 var util = require('../lib/utility');
 
+var isEmpty = function(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
+};
+
 var createSession = function(req, res, next) {
   // console.log('request', req);
-  console.log('response', res.cookie);
+  // console.log('response', res.cookie);
   console.log('And cookie is for me ', req.cookies);
   // res.cookie = req.cookies[hash[0].trim()] = hash[1];
-  req.session = {hash: 0};
+  // res.cookie(req.cookies[], req.cookies[1]);
+  if (isEmpty(req.cookies)) {
+    req.session = {hash: 0};
+    // res.cookie('hash', 0);
+  } else {
+    for (var name in req.cookies) {
+      console.log('name', name);
+      res.cookie(name, req.cookies[name]);
+    }
+    console.log('fred', req);
+  }
   next();
 };
 
