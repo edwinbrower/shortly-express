114a0b852c07695e5471475a49cbe60c8e97bdb2
edwinbrower <edwin.brower@gmail.com>
2017-03-06 19:38:12 -0800

    Insert user name properly hashes password

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,14 @@
 var db = require('../db');
 var utils = require('../lib/utility');
+var crypto = require('crypto');
 
 // Write you user database model methods here
 
 var insertId = function(userParams) { 
   console.log('addUser model');
-
-  // TO DO HASH PASSWORD BEFORE STORING
+  var shasum = crypto.createHash('sha1');
+  shasum.update(userParams[1]);
+  userParams[1] = shasum.digest('hex');
   var queryString = 'INSERT INTO users (username, password)\
                       VALUES (?, ?)';
   return db.queryAsync(queryString, userParams);
